a764a89ac8ea78c2bad6b37ffce6c6d8c09c53cb
gilgamesh7 <felidaesrule@yahoo.com>
2022-06-27 20:16:49 +1200

    Add simple downtime - needs variable changes

--- sla/views.py
+++ sla/views.py
@@ -8,17 +8,23 @@ import requests
 def simple_sla(request):
     simple_sla_data = {}
 
-    # if 'simple_uptime' in request.GET:
-    #     try:
-    #         logger.info("Get SLA for simple Uptime as input")
-    #         simple_sla = request.GET['simple_uptime']
-    #         with requests.Session() as session:
-    #             response = session.get('https://get.uptime.is/api', params={'sla': simple_sla})
-    #             response.raise_for_status()
+    if 'simple_uptime_hours' in request.GET:
+        try:
+            hours = int(request.GET['simple_uptime_hours'])
+            minutes = int(request.GET['simple_uptime_minutes'])
+            seconds = int(request.GET['simple_uptime_seconds'])
 
-    #         simple_downtime_data = response.json()
-    #     except Exception as err:
-    #         logger.error(f"Exception : {err}")
+            request_parameter = f"{hours}h{minutes}m{seconds}s"
+
+            logger.info(f"Get Uptime for simple Uptime as input : {request_parameter}")
+
+            with requests.Session() as session:
+                response = session.get('https://get.uptime.is/api', params={'down': request_parameter})
+                response.raise_for_status()
+
+            simple_sla_data = response.json()
+        except Exception as err:
+            logger.error(f"Exception : {err}")
 
     return render(request, 'sla/simple_sla.html', {'simple_sla_data':simple_sla_data})
 
